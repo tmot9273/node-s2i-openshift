4996693550098403b3e9815091ee22ba6ae05005
tmot9273 <68030369+tmot9273@users.noreply.github.com>
2020-07-08 16:07:39 -0400

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,12 +52,12 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
+        "name": "Ralph Demetry",
         "age": 38,
-        "gender": "male",
-        "street": "34 Main Street",
+        "gender": "Unknown",
+        "street": "21 Main Street",
         "city": "Toronto",
-        "zipcode": "M5H 1T1"
+        "zipcode": "M5H 2S4"
       },
       "medications": ["Metoprolol", "ACE inhibitors", "Vitamin D"],
       "appointments": ["2018-01-15 1:00 - Dentist", "2018-02-14 4:00 - Internal Medicine", "2018-09-30 8:00 - Pediatry"]
